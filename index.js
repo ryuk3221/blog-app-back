d334bd1507d55193437a540b326fa7981c8db0ee
Danil <danilmitrofanov123@gmail.com>
2024-10-31 18:29:20 +0300

    Подключил mongoDB, доабвил User модель, реализовал валидацию при регистрации

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+
+const db_url = 'mongodb+srv://danilmitrofanov123:map2253377@cluster0.gqygo.mongodb.net/';
+mongoose
+  .connect(db_url)
+  .then(res => {
+    console.log('db OKAY')
+  })
+  .catch(error => {
+    console.log('Error connect', error);
+  })
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send(JSON.stringify({status: "Success", message: "Hello world"}));
 });
 
-app.post('/auth/login', (req, res) => {
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
 
-  res.json(req.body);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  res.json({
+    success: true
+  });
 });
 
 app.listen(4444, (err) => {
---
+++ models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+//создаю схему таблицы User
+const UsersSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  avatarUrl: String
+},{
+  timestamps: true,
+});
+
+//создаю модель данной таблицы и экспортирую
+export default mongoose.model('User', UsersSchema);
