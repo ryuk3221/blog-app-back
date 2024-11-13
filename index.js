3a28ae156773681e86ef6511ec1ba905acd93d55
Danil <danilmitrofanov123@gmail.com>
2024-11-13 18:16:50 +0300

    Добавил middlewhare для отключения cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import multer from "multer";
 import { registerValidation } from "./validations/auth.js";
 import checkAuth from "./utils/checkAuth.js";
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 
